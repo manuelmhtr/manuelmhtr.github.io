2ecbc16fd2a6c4f990d07ac521319686f326590b
Manuel de la Torre <manuelmhtr@gmail.com>
2020-03-28 15:17:47 -0600

    v1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Manuel de la Torre
+
+A simple site to introduce me.
+
+### Attribution
+
+* Icons: [Ionicons](https://ionicons.com)
+* UI: [Tailwindcss](https://tailwindcss.com/)
+* Template: [Sivanesh-S](https://github.com/Sivanesh-S/portfolio-minimal/)
---
+++ data.js
@@ -0,0 +1,48 @@
+const mail = 'manuelmhtr@gmail.com';
+
+const main = {
+  mail,
+  name: 'Manuel de la Torre 👋',
+  img: './assets/profile-pic.jpg',
+  role: ['Product maker'],
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/manuelmhtr'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/manuelmhtr'
+    },
+    {
+      name: 'StackOverflow',
+      iconName: 'logo-stackoverflow',
+      link: 'https://stackoverflow.com/users/1628009/manuelmhtr'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/manuelmhtr'
+    },
+    {
+      name: 'NPM',
+      iconName: 'logo-npm',
+      link: 'https://www.npmjs.com/~manuelmhtr'
+    },
+    {
+      name: 'Instagram',
+      iconName: 'logo-instagram',
+      link: 'https://www.instagram.com/manuelmhtr/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${mail}?Subject=Hello`
+    }
+  ],
+  links: [
+    /* { name: 'Projects', link: '#projects' }, */
+  ]
+};
